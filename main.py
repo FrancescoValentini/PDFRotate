4092a24239d4b5322dc51f39a6fb401bb8d9486c
Francesco Valentini <Francesco.valentini930@gmail.com>
2025-03-15 12:37:28 +0100

    Metodo per la compressione di un PDF e argomenti CLI
    Modificato anche il metodo per ruotare un pdf, entrambi ora utilizzano
    la libreria PyMuPDF

--- main.py
+++ main.py
@@ -1,20 +1,46 @@
-from PyPDF2 import PdfReader, PdfWriter
+import argparse
+import fitz  # PyMuPDF
 
 def rotate(inputFile, outputFile, angle=90):
     """
-    Rotates a PDF file by a specified angle and saves the rotated version to a new file.
+    Rotates all pages of a PDF file by a specified angle and saves the rotated version to a new file.
 
     :param inputFile: Path to the input PDF file that needs to be rotated.
     :param outputFile: Path where the rotated PDF will be saved.
-    :param angle: The angle by which to rotate the PDF pages. Default is 90 degrees.
+    :param angle: The angle by which to rotate the PDF pages. Must be 90, 180, or 270 degrees.
     :return: None
     """
-    reader = PdfReader(inputFile)
-    writer = PdfWriter()
+    doc = fitz.open(inputFile)
 
-    for page_index in range(len(reader.pages)):
-        page = reader.pages[page_index]
-        writer.add_page(page.rotate(angle))
+    for page in doc:
+        page.set_rotation(angle)
 
-    with open(outputFile, "wb") as fp:
-        writer.write(fp)
+    doc.save(outputFile)
+    doc.close()
+
+
+def compress(inputFile, outputFile):
+    """
+    Compresses a PDF file
+
+    :param inputFile: Path to the input PDF file that needs to be compressed.
+    :param outputFile: Path where the compressed PDF will be saved.
+    :return: None
+    """
+    doc = fitz.open(inputFile)
+
+    # Save compressed file
+    doc.save(outputFile, garbage=4, deflate=True, clean=True)
+    doc.close()
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("inputFile")
+    parser.add_argument("outputFile")
+    parser.add_argument("-d", "--degrees", action="store_const")
+    parser.add_argument("-c", "--compress", action="store_true")
+    args = parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
